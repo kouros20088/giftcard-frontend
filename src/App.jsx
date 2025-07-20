b6f74ad8dbee96e7f900b05a3bcd7d03bf366646
kouros2008 <ramtinkouros37@gmail.com>
2025-07-20 13:36:39 +0100

    app.jsx updat 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
+// src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Form from './pages/Form';
 import Success from './pages/Success';
 import ErrorPage from './pages/ErrorPage';
+import Footer from './components/Footer';
+import './App.css';
 
 function App() {
   return (
     <Router>
-      <div className="app">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/form" element={<Form />} />
-          <Route path="/success" element={<Success />} />
-          <Route path="/error" element={<ErrorPage />} />
-        </Routes>
-        <Footer />
-      </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/form" element={<Form />} />
+        <Route path="/success" element={<Success />} />
+        <Route path="/error" element={<ErrorPage />} />
+      </Routes>
+      <Footer />
     </Router>
   );
 }
