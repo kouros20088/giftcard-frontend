7367963227145143082608d8465c4435236c1937
kouros2008 <ramtinkouros37@gmail.com>
2025-07-20 13:35:16 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Form from './pages/Form';
 import Success from './pages/Success';
-import Error from './pages/Error';
+import ErrorPage from './pages/ErrorPage';
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/form" element={<Form />} />
-        <Route path="/success" element={<Success />} />
-        <Route path="/error" element={<Error />} />
-      </Routes>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/form" element={<Form />} />
+          <Route path="/success" element={<Success />} />
+          <Route path="/error" element={<ErrorPage />} />
+        </Routes>
+        <Footer />
+      </div>
     </Router>
   );
 }
